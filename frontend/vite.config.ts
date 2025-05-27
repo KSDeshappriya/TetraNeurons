fcb2668efe034180d9d977019411a41f1dbf3869
KSDeshappriya <ksdeshappriya.official@gmail.com>
2025-05-27 11:17:09 +0530

    https

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import fs from 'fs'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,5 +11,10 @@ export default defineConfig({
   server: {
     host: '0.0.0.0', // Listen on all addresses, including LAN
     port: 5173,      // Or any port you like
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'key/key.pem')), // Path to your private key
+      cert: fs.readFileSync(path.resolve(__dirname, 'key/cert.pem')), // Path to your certificate
+      passphrase: '1234'
+    },
   },
 })
